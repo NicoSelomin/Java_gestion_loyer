5b30e602a5af67658b17b6d403fe7e0dcafb02aa
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-05-25 22:46:28 -0400

    add Paiement and Quitance entities for payment system

---
+++ src/com/nicoselomin/gestionLoyer/domain/Paiement.java
@@ -0,0 +1,10 @@
+package com.nicoselomin.gestionLoyer.domain;
+import java.util.Date;
+
+public class Paiement {
+    private Date mois;
+    private float montantPaye;
+    private Date datePaiement;
+    private ModePaiement modePaiement;
+    private StatutPaiement statut;
+}
---
+++ src/com/nicoselomin/gestionLoyer/domain/Quittance.java
@@ -0,0 +1,8 @@
+package com.nicoselomin.gestionLoyer.domain;
+import java.util.Date;
+
+public class Quittance {
+    private String lien;
+    private String reference;
+    private Date dateGeneration;
+}
