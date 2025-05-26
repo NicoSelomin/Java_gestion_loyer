7bbd7e5fc22544af3754e3cf76151059807e989f
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-05-25 22:45:42 -0400

    feat : define ContratLocation entity with fields

---
+++ src/com/nicoselomin/gestionLoyer/domain/ContratLocation.java
@@ -0,0 +1,13 @@
+package com.nicoselomin.gestionLoyer.domain;
+import java.util.Date;
+
+public class ContratLocation {
+    private String reference;
+    private Date dateEntree;
+    private float loyerEtCharges;
+    private float depotDeGarantie;
+    private int jourDePaiement;
+    private Date dateSortie;
+    private EtatContrat etat;
+    private String lien;
+}
