a8e4f1450c53192a37f12eb5ab83a00b86e889b0
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2025-05-26 20:14:45 -0400

    feat: add BienImmobilier entity

---
+++ src/com/nicoselomin/gestionLoyer/domain/BienImmobilier.java
@@ -0,0 +1,10 @@
+package com.nicoselomin.gestionLoyer.domain;
+
+public class BienImmobilier {
+    private String designation;
+    private String adresse;
+    private String type;
+    private float loyerMensuel;
+    private float chargeMensuel;
+    private String statut;
+}
